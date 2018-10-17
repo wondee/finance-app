b5f496c9f1b7b69037a12680cf3daa59ba394351
wondee <markuswondrak@gmail.com>
2018-10-17 19:46:01 +0200

    changed detailview to ajax request

--- finance-application/src/main/java/info/wondee/app/financeapp/config/WebConfig.java
+++ finance-application/src/main/java/info/wondee/app/financeapp/config/WebConfig.java
@@ -1,10 +1,13 @@
 package info.wondee.app.financeapp.config;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -26,14 +29,18 @@ public class WebConfig implements WebMvcConfigurer {
   
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-      registry
-        .addResourceHandler("/webjars/**")
-        .addResourceLocations("/webjars/");
-      
-      registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/static/");
-      
-      registry.addResourceHandler("/.well-known/acme-challenge/**").addResourceLocations("classpath:/static/");
-      
+
+//    VersionResourceResolver versionResourceResolver = new VersionResourceResolver()
+//        .addVersionStrategy(new ContentVersionStrategy(), "/**");
+ 
+      // TODO add cache control
+    registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/static/")
+      .setCacheControl(CacheControl.maxAge(5, TimeUnit.DAYS));
+//      .resourceChain(true)
+//      .addResolver(versionResourceResolver);
+    
+    registry.addResourceHandler("/.well-known/acme-challenge/**").addResourceLocations("classpath:/static/");
+    
   }
   
   public void addViewControllers(ViewControllerRegistry registry) {
---
+++ finance-application/src/main/resources/static/overview.js
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+
+var app = new Vue(
+		
+	{
+	  el: '#overview-app',
+	  data: {
+	    month: "No Month",
+	    
+	    specialCosts: Array(),
+	    fixedCosts: Array()
+	  },
+	  methods: {
+		  showModal: function(event) {
+			  var link = event.target.parentElement;
+			  var index = link.getAttribute('index');
+			  this.month = link.getAttribute('month');
+
+			  $('#details-modal').modal('show');
+			  $('#load_indicator').show();
+			  
+			  // GET /someUrl
+			  this.$http.get('/overview/detail', {params: {'index': index}}).then(
+				function(response) {
+			    // success callback
+				  console.log(response.data);
+				  
+				  this.specialCosts = response.data.specialCosts;
+				  this.fixedCosts = response.data.fixedCosts;
+				  
+				  $('#load_indicator').hide();
+				  
+			  }, function(response) {
+			    // error callback
+			  });
+			  
+		  }
+	  }
+	}
+
+);
