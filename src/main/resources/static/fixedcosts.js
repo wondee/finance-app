f0d53b4c9b7f225db2ac64aea07e1548deb0cc43
Markus <markuswondrak@googlemail.com>
2018-10-26 07:30:26 +0000

    added modal for limited view in mobile

---
+++ src/main/resources/static/fixedcosts.js
@@ -0,0 +1,24 @@
+var app = new Vue(
+
+	{
+	  el: '#fixedcosts-app',
+	  data: {
+	    name: "No Name",
+
+	    from: "-",
+	    to: "-"
+      },
+
+      methods: {
+          showModal(index, elementId) {
+
+            var row = getElmentById(elementId).rows[index];
+
+            this.name = row.cells[0].text();
+            this.from = row.cells[2].text();
+            this.to = row.cells[3].text();
+
+            $('#limited-modal').modal('show');
+          }
+      }
+     });
