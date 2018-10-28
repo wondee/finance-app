0b78e9892ccfa35d9730c33066fcf2a355cf9bfc
wondee <markuswondrak@gmail.com>
2018-10-28 18:41:44 +0100

    changed domain

--- deploy/create_cert.cmd
+++ deploy/create_cert.cmd
@@ -2,3 +2,6 @@ docker-compose run --rm letsencrypt \
   letsencrypt certonly --webroot \
   --email markus.wondrak@gmail.org --agree-tos \
   -w /var/www/letsencrypt -d finance-vm.wondee.info
+
+
+docker-compose kill -s SIGHUP nginx
