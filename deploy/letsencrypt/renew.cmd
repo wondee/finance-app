faa2f2f52da02fbb650966477089842b5b86d087
unknown <markuswondrak@gmail.com>
2019-04-19 14:09:58 +0200

    tweak cert update script

--- deploy/letsencrypt/renew.cmd
+++ deploy/letsencrypt/renew.cmd
@@ -1,6 +1,7 @@
 
 
-sudo certbot certonly --webroot -w /var/lib/docker/volumes/deploy_letsencrypt_www/_data -d finance.wondee.inf o -d jenkins.wondee.info
+sudo certbot certonly --webroot -w /var/lib/docker/volumes/deploy_letsencrypt_www/_data -d finance.wondee.info -d jenkins.wondee.info
+sudo su
 cp -L /etc/letsencrypt/live/finance.wondee.info/* /var/lib/docker/volumes/deploy_letsencrypt_certs/_data/live/finance.wondee.info/
 
 sudo docker-compose kill -s SIGHUP nginx
